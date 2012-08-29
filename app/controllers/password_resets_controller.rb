7b7c12bad5f9617d17000f0bc1cc59e92ca2b0ab
Eli Fatsi <efatsi@comcast.net>
2012-08-29 14:18:32 -0600

    controller modified for refactoring

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,7 @@
 class PasswordResetsController < ApplicationController
   
+  before_filter :assign_password_reset, :only => [:edit, :update]
+  
   def new
     @password_reset = PasswordReset.new
   end                              
@@ -15,24 +17,18 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
-    assign_password_reset
     if @password_reset.expired?
       redirect_to new_password_reset_path, :alert => "Password reset has expired"
     end
   end
 
   def update
-    assign_password_reset
-    if @password_reset.update_attributes(params[:password_reset])
+    if @password_reset.save_new_password(params[:password_reset])
       session[:user_id] = @password_reset.user.id
       redirect_to root_url, :notice => "Password has been reset, hurray!"
     else
       render :edit
     end
-  end
-  
-  def password_reset
-    @password_reset
   end
 
   private
