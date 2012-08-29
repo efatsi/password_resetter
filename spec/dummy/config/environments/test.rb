b47199ff46f884ceead5629867ba7b97c6627e9e
Eli Fatsi <efatsi@comcast.net>
2012-08-29 11:24:43 -0600

    all tests pass

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -34,4 +34,8 @@ Dummy::Application.configure do
 
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
+  
+  
+  # Action mailer for reset passwords
+  config.action_mailer.default_url_options = { :host => "localhost:3000" }
 end
