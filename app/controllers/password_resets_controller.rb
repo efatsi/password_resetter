e873d0950e5afd1771ba16bd36d6502f65c9d27a
Eli Fatsi <efatsi@comcast.net>
2012-08-30 10:38:20 -0600

    fixed update bug

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,6 +1,5 @@
 class PasswordResetsController < ApplicationController
   
-  before_filter :assign_password_reset, :only => [:edit, :update]
   
   def new
     @password_reset = PasswordReset.new
@@ -9,7 +8,7 @@ class PasswordResetsController < ApplicationController
   def create                       
     @password_reset = PasswordReset.new(params[:password_reset])
     if @password_reset.match_identifier? && @password_reset.save
-      @password_reset.send_password_reset ### PROBLEM IS RIGHT HERE!!
+      @password_reset.send_password_reset
       redirect_to root_url, :notice => "Email sent with password reset instructions."
     else
       redirect_to new_password_reset_path, :alert => "Could not find a user to match :("
@@ -17,23 +16,20 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+    @password_reset = PasswordReset.find_by_reset_token(params[:id])
     if @password_reset.expired?
       redirect_to new_password_reset_path, :alert => "Password reset has expired"
     end
   end
 
   def update
+    @password_reset = PasswordReset.find(params[:id])
     if @password_reset.update_user_password(params[:password_reset][:password], params[:password_reset][:password_confirmation])
       session[:user_id] = @password_reset.user.id
       redirect_to root_url, :notice => "Password has been reset, hurray!"
     else
       render :edit
     end
-  end
-
-  private
-  def assign_password_reset
-    @password_reset = PasswordReset.find_by_reset_token(params[:id])
   end
   
 end
