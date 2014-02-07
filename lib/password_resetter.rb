ccd39514f9b0a2b57e7349617c140bb0d91c15a0
Eli Fatsi <eli.fatsi@viget.com>
2014-02-06 20:00:15 -0700

    Version bump to 1.0.9

--- lib/password_resetter.rb
+++ lib/password_resetter.rb
@@ -3,4 +3,8 @@ require "password_resetter/engine"
 module PasswordResetter
   mattr_accessor :from_email
   @@from_email = "password.resetter@no-reply.com"
+
+  def self.setup
+    yield self
+  end
 end
--- lib/password_resetter/version.rb
+++ lib/password_resetter/version.rb
@@ -1,3 +1,3 @@
 module PasswordResetter
-  VERSION = "1.0.7"
+  VERSION = "1.0.9"
 end
